a0215f79c6861f6e6f9904376f2123858c2488b5
Akshay Priyadarshi <akshayp1904@outlook.com>
2022-07-22 13:06:59 +0530

    added models

--- main/admin.py
+++ main/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-# from .models import Farm, Farmer, Fertilizer,Crop, Schedule
+from .models import Farm, Farmer, Fertilizer,Crop, Schedule
 
-# # Register your models here.
+# Register your models here.
 
-# admin.site.register(Farmer)
-# admin.site.register(Farm)
-# admin.site.register(Crop)
-# admin.site.register(Fertilizer)
-# admin.site.register(Schedule)
+admin.site.register(Farmer)
+admin.site.register(Farm)
+admin.site.register(Crop)
+admin.site.register(Fertilizer)
+admin.site.register(Schedule)
