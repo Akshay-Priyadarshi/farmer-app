afc6d10aa752a9bb78f29f4a8dba07b88a3c62a6
Akshay Priyadarshi <akshayp1904@outlook.com>
2022-07-24 12:48:29 +0530

    added swagger

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
 from . import views
 
 router = routers.SimpleRouter()
@@ -12,6 +14,11 @@ router.register(r'schedules', views.ScheduleViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('api-schema/',get_schema_view(title='Farmerz API Schema'),name='api-schema'),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url':'api-schema'}
+    ), name='swagger-ui'),
     path('farmerswithcrops/', views.farmerswithcrops, name='farmerswithcrops'),
     path('farmerschedule/<uuid:farmer_id>/', views.farmerschedule, name='farmerschedule'),
     path('totalcost/<uuid:farmer_id>/', views.totalCost, name='totalcost'),
